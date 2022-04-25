f8c46a494dfb98dbbb85af7d2df0652ef1c59a9e
LeoTexx <leonardo@mistho.io>
2022-04-25 04:13:29 +0100

    remove job

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,24 @@
 import { Job } from "../types/api";
 import style from "../styles/Card.module.scss";
-import { Button, Text } from "@textkernel/oneui";
+import { Button, StepperButton } from "@textkernel/oneui";
 import { memo } from "react";
 
 interface Props {
   job: Job;
   onSelect: (job: Job) => void;
+  onDelete: (job: Job) => void;
 }
 
-function CardComponent({ job, onSelect }: Props) {
+function CardComponent({ job, onSelect, onDelete }: Props) {
   return (
     <div className={style.container}>
+      <div className={style.removeJob}>
+        <StepperButton
+          disabled={false}
+          icon="minus"
+          onClick={() => onDelete(job)}
+        />
+      </div>
       <h1>{job.job_title}</h1>
       <h3>{job.organization_name}</h3>
       <p>
